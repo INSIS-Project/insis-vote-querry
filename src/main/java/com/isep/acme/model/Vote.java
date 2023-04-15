f8380a15aa03ecad50f311ed9725b54a968a39bc
rainanaraujo <rainanaraaujo@gmail.com>
2023-04-15 06:33:39 -0300

    initial commit

---
+++ src/main/java/com/isep/acme/model/Vote.java
@@ -0,0 +1,45 @@
+package com.isep.acme.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.isep.acme.model.enumerate.voteType;
+import com.isep.acme.services.dto.VoteDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long voteID;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private voteType voteType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    public VoteDTO toDto() {
+        return new VoteDTO(this.voteID,
+                this.voteType,
+                this.review.getIdReview());
+    }
+
+}
